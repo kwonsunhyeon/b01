88bfedc73b17633ac9b919eb83cc4d550b56130b
java-oop <topsh90@gmail.com>
2024-04-09 14:21:53 +0900

    Commit: Thymeleaf [setting, start] ~Search by keyword 1

--- src/main/java/hello/b01/repository/BoardRepository.java
+++ src/main/java/hello/b01/repository/BoardRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hello.b01.domain.Board;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
+public interface   BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
 
     @Query(value = "select now()", nativeQuery = true)
     String getTime();
