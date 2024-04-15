657dd6f144546845fad58cba1df9a73f351c27de
java-oop <topsh90@gmail.com>
2024-04-15 11:28:06 +0900

    swagger 6-3 완

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -9,7 +9,13 @@ async function get1(bno) {
 
 async function getList({bno, page, size, goLast}){
     const result = await axios.get(`/replies/list/${bno}`,{params:{page, size}})
-    // console.log(result)
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+
+        return getList({bno:bno, page:lastPage, size:size})
+
+    }
     return result.data
 }
 async function addReply(replyObj) {
@@ -22,5 +28,9 @@ async function getReply(rno){
 }
 async function modifyReply(replyObj){
     const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
+    return response.data
+}
+async function removeReply(rno) {
+    const response = await axios.delete(`/replies/${rno}`)
     return response.data
 }
