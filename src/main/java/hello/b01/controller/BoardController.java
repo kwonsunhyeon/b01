4291bc9944c3fe8f105c461653d904b9885d4e5f
java-oop <topsh90@gmail.com>
2024-04-17 17:19:55 +0900

    8-1 710까지 완!

--- src/main/java/hello/b01/controller/BoardController.java
+++ src/main/java/hello/b01/controller/BoardController.java
@@ -7,6 +7,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,12 +20,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 @Tag(name="board", description = "게시물 관련 API")
 public class BoardController {
+
+    @Value("${hello.upload.path")     //import 시에 springframework로 시작하는 Value
+    private String uploadPath;
 
     private final BoardService boardService;
 
@@ -38,34 +49,35 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO);
     }
 
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
-    public void registerGET(){
+    public void registerGET() {
 
     }
 
 
     @PostMapping("/register")
     @Operation(summary = "새로운 게시물을 추가한다.")
-    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         log.info("board POST register.......");
 
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             log.info("has errors.......");
-            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors() );
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
             return "redirect:/board/register";
         }
 
         log.info(boardDTO);
 
-        Long bno  = boardService.register(boardDTO);
+        Long bno = boardService.register(boardDTO);
 
         redirectAttributes.addFlashAttribute("result", bno);
 
         return "redirect:/board/list";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping({"/read", "/modify"})
     @Operation(summary = "하나의 게시물을 읽습니다.")
     public void read(@RequestParam("bno") Long bno, PageRequestDTO pageRequestDTO, Model model) {
@@ -109,14 +121,50 @@ public class BoardController {
 
     @PostMapping("/remove")
     @Operation(summary = "게시물 하나를 삭제합니다.")
-    public String remove(@RequestParam("bno") Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+
+        Long bno = boardDTO.getBno();
 
         log.info("remove post..." + bno);
 
         boardService.remove(bno);
 
+        //게시물이 데이터베이스상에서 삭제 되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
+
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
     }
+
+    public void removeFiles(List<String> files){
+
+        for (String fileName:files) {
+
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+            String resourceName = resource.getFilename();
+
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+
+                //섬네일이 존재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                    thumbnailFile.delete();
+                }
+
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+
+        }//end for
+    }
+
 }
+
